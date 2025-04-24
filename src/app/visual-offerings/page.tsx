47dd1f9321ca75309f02ec597464a25c5aa8863f
Chris <chrisrobholmes@gmail.com>
2025-04-23 20:41:32 -0400

    update visual offerings page

--- src/app/visual-offerings/page.tsx
+++ src/app/visual-offerings/page.tsx
@@ -1,11 +1,13 @@
-import Link from "next/link";
-import Image from "next/image";
+import visualOfferingsData from "./visualOfferingsData";
+import ImageGridWithModal from "../components/ImageGridWithModal";
 
 export default function About() {
   return (
-    <div>
+    <div className="py-8">
       <div>
-        <h1>Visual Offerings</h1>
+        <h1 className="text-4xl font-bold mb-4 text-jhcteal-100">
+          Visual Offerings
+        </h1>
         <p>
           Whether you present at conferences or webinars or facilitate group
           process, you can add value by including a graphic visual or ‘map’ to
@@ -18,306 +20,28 @@ export default function About() {
           impact. Click on the buttons below to see some examples.
         </p>
       </div>
-      <div>
-        <ul>
-          <li>
-            <h3>You are a Presenter</h3>
-            <Link key={"/visual-offerings"} href={"/visual-offerings"}>
-              Explore Visual Offerings
-            </Link>
-          </li>
-          <li>
-            <h3>You are a Facilitator</h3>
-            <Link key={"/visual-offerings"} href={"/visual-offerings"}>
-              Explore Visual Offerings
-            </Link>
-          </li>
-          <li>
-            <h3>You are a Leader</h3>
-            <Link key={"/visual-offerings"} href={"/visual-offerings"}>
-              Explore Visual Offerings
-            </Link>
-          </li>
-          <li>
-            <h3>You are a Learning Professional</h3>
-            <Link key={"/visual-offerings"} href={"/visual-offerings"}>
-              Explore Visual Offerings
-            </Link>
-          </li>
+      <div className="py-6">
+        <ul className="grid grid-cols-1 md:grid-cols-4 gap-4 text-center">
+          {visualOfferingsData.map((offering) => (
+            <li className="py-3 " key={offering.id}>
+              <h3 className="pb-4 font-semibold">{offering.title}</h3>
+              <a
+                href={`#${offering.id}`}
+                className="bg-transparent hover:bg-jhcteal-100 text-jhcteal-100 font-semibold hover:text-white py-2 px-4 border border-jhcteal-100 hover:border-transparent rounded"
+              >
+                Explore Visual Offerings
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
-      <div>
-        <h3>Maps supporting you as a Presenter</h3>
-        <p>
-          As a presenter, you tell your story with a call for action. You want
-          people to remember your key messages.{" "}
-        </p>
-        <ul>
-          <li>
-            <Image
-              src="/vo-rebecca-sutherns-zoom-but-better.jpg"
-              alt="Visual map for Rebecca Sutherns' webinar"
-              width={500}
-              height={500}
-            />
-            <p>
-              Rebecca Sutherns offered a webinar of tips and advice to improve
-              Zoom facilitation skills. This map represented much of her sage
-              advice.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-michael-bungay-stanier.jpg"
-              alt="Visual map for Michael Bungay Stanier's video series"
-              width={500}
-              height={500}
-            />
-            <p>
-              This image represents Michael Bungay Stanier’s key messages from a
-              video for his new book, The Advice Trap. This map was 1 of 5
-              created to support his video series. He sent these maps as part of
-              a follow-up email to the 2,000 video subscribers.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-jackie-lauer.jpg"
-              alt="Visual map for Jackie Lauer’s resiliency presentation"
-              width={500}
-              height={500}
-            />
-            <p>
-              I captured Jackie Lauer’s key messages on Resiliency from her
-              presentation in a Zoom call. She provided this map to all
-              participants as a take-away. The map also serves as a ‘business
-              card’ for future opportunities for Jackie.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-nancy-priest.jpg"
-              alt="Sketchnote for Nancy Priest’s presentation"
-              width={500}
-              height={500}
-            />
-            <p>
-              Nancy Priest presented to learning professionals and this
-              sketchnote provided a living example of ‘transfer of learning’ for
-              participants to retain her key messages.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-frank-newman.jpg"
-              alt="Visual summary for Frank Newman's webinar"
-              width={500}
-              height={500}
-            />
-            <p>
-              Frank Newman provided this map as a summary for his webinar
-              participants. He posted in on his website as promotion.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-danielle-wallace.jpg"
-              alt="Sketchnote for Danielle Wallace’s presentation"
-              width={500}
-              height={500}
-            />
-            <p>
-              Danielle Wallace presented “Advertising Techniques for Compelling
-              Learning Videos” at an online conference through Zoom. She used
-              this map to promote her content on LinkedIn.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-inspiring-women.jpg"
-              alt="Sketchnote for 2019 Inspiring Women conference keynote speakers"
-              width={500}
-              height={500}
-            />
-            <p>
-              It was a delight to sketchnote the keynote speakers of the 2019
-              Inspiring Women conference.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-emily-obrien.jpg"
-              alt="Sketchnote for Emily O'Brien’s story"
-              width={500}
-              height={500}
-            />
-            <p>
-              I translated Emily O&#39;Brien&#39;s story into a map providing
-              the F#@kUp audience a takeaway and reminder of her powerful life
-              lessons.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-church-sermon.jpg"
-              alt="Visual notes for online Church sermon"
-              width={500}
-              height={500}
-            />
-            <p>
-              I supplemented an online Church sermon with a map of key points
-              helping reinforce the messages. This was posted along with the
-              video recording on the Church’s FB site.
-            </p>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h3>Maps supporting you as a Facilitator</h3>
-        <p>
-          As a facilitator, you want graphic recording to support the group
-          process and outcome - whether virtual or in person. A map summarizes
-          key points, highlights of break-out group reports and decisions
-          towards next steps. I would work closely with you as you plan the
-          agenda to support the group moving forward.
-        </p>
-        <ul>
-          <li>
-            <Image
-              src="/vo-all-staff-meeting.jpg"
-              alt="Map of All Staff Meeting highlights"
-              width={500}
-              height={500}
-            />
-            <p>
-              Highlights of this All Staff Meeting were captured on this one
-              map. Includes key content from the morning training session and
-              the highlights of the afternoon facilitated break-out group
-              activity – report-backs of how to demonstrate the newly launched
-              concepts in daily work.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-first-zoom-meeting.jpg"
-              alt="Map capturing first group Zoom meeting"
-              width={500}
-              height={500}
-            />
-            <p>
-              A map of this group’s first Zoom meeting included highlights of
-              members’ ‘check-in’ activity early in the Pandemic and
-              report-backs from Break-Out rooms.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-facilitator-question.jpg"
-              alt="Map showing breakout room responses and next steps"
-              width={500}
-              height={500}
-            />
-            <p>
-              The facilitator used Break-Out rooms to solicit ideas from 1
-              single question. The Report-backs were captured in this map in
-              addition to the Group’s next steps.
-            </p>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h3>Maps supporting you as a Leader</h3>
-        <p>
-          As a leader of a team or organization, you want to capture the
-          attention of current and potential stakeholders, employees and clients
-          by engaging them in the big picture of your strategy and/or services.
-          An overview map can attract their interest and consideration of your
-          story.
-        </p>
-        <ul>
-          <li>
-            <Image
-              src="/vo-carizon-strategic-map.jpg"
-              alt="Digital map of Carizon’s strategic plan"
-              width={500}
-              height={500}
-            />
-            <p>
-              This was my first digital map of an organization’s strategic plan,
-              targeted to engage employees. Carizon laminated a map for every
-              employee with highlights of each of the 4 Strategic Initiatives on
-              the back. Managers and employees discussed the link of their
-              specific work connected to the strategic initiatives.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-greenway-chaplin-update.jpg"
-              alt="Updated strategic map for Greenway-Chaplin Community Centre"
-              width={500}
-              height={500}
-            />
-            <p>
-              We updated the original Greenway-Chaplin Community Centre
-              strategic map for fundraising and soliciting volunteers to reflect
-              the drastic change of delivery given COVID-19.
-            </p>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h3>Maps supporting you as a Learning Professional</h3>
-        <p>
-          As a learning professional, you want to create a visual map to promote
-          the objectives and/or a map of highlights to support memory retention,
-          transfer of learning and reinforcement. Consider whimsical graphics to
-          supplement your presentation within the session.
-        </p>
-        <ul>
-          <li>
-            <Image
-              src="/vo-good-stuff-coaching.jpg"
-              alt="Leadership Good Stuff e-Course visual map"
-              width={500}
-              height={500}
-            />
-            <p>
-              Good Stuff Coaching offers a map for download for each video
-              lesson in the Leadership Good Stuff e-Course. Learners are
-              directed to make this summary their own to help them to apply the
-              learnings. Wonderful ‘transfer of learning’ practice.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-growth-mindset-workshop.jpg"
-              alt="Infographic and custom map for Growth Mindset workshop"
-              width={500}
-              height={500}
-            />
-            <p>
-              I still use this non-digital infographic as a foundational
-              learning aid in my Growth Mindset workshops. I then summarize with
-              a colourful customized digital map of all the ideas the group
-              comes up with for shifting to a Growth Mindset for each of the 5
-              practices.
-            </p>
-          </li>
-          <li>
-            <Image
-              src="/vo-disc-productive-conflict.jpg"
-              alt="Visual map of Everything DiSC Productive Conflict"
-              width={500}
-              height={500}
-            />
-            <p>
-              From watching a Zoom webinar, I summarized the key elements of how
-              the Everything DiSC Productive Conflict resource transforms
-              workplace conflict. I use this map to promote my DiSC workshops.
-            </p>
-          </li>
-        </ul>
-      </div>
+      {visualOfferingsData.map((offering) => (
+        <div id={offering.id} className="py-8" key={offering.id}>
+          <h3 className="text-xl font-bold mb-4">{offering.title}</h3>
+          <p className="mb-4">{offering.description}</p>
+          <ImageGridWithModal images={offering.images} />
+        </div>
+      ))}
     </div>
   );
 }
